2bd7bf1901a008f879be2c1924e53a252e6d9c7a
SKotapati <SKotapati@shawsystems.com>
2023-08-08 14:25:50 +0000

    sample program

---
+++ src/program/SampleProgram.java
@@ -0,0 +1,16 @@
+package com.program;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		boolean value = false;
+		
+		if(value) {
+			System.out.println("Value is true");
+		} else {
+			System.out.println("Value is false");
+		}
+
+	}
+
+}
